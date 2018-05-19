03383a89f547a8269ecad6a31bdfb10ed0d62a32
rodrigo22 <digoqueiroz19@gmail.com>
2018-05-19 01:04:17 +0000

    minha versão depois da aula

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
@@ -10,12 +10,19 @@ public class Emissor {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri("amqp://...");
+    //fornece credenciais de acesso
+    factory.setUri("amqp://intncmcf:UyFMaUKymdXZhqKOlihaiKyV6lcn9mI9@mosquito.rmq.cloudamqp.com/intncmcf");
+    //conexão.
     Connection connection = factory.newConnection();
+    //Partilhar a conexão tcp. canais lógicos dentro da conexao tcp, pode criar quantos quiser. a conexão tcp é uma só
     Channel channel = connection.createChannel();
 
+    //dentro do canal que acontece a interação com o rabbitMQ. ver na documentação os parametros.
+    //criação da fila.
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    
     String message = "Olá!!!";
+    //mensagem vai para a fila.
     channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
     System.out.println(" [x] Mensagem enviada: '" + message + "'");
 
--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
@@ -10,20 +10,26 @@ public class Receptor {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri("amqp://...");
+    //credenciais
+    factory.setUri("amqp://intncmcf:UyFMaUKymdXZhqKOlihaiKyV6lcn9mI9@mosquito.rmq.cloudamqp.com/intncmcf");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
+    //declarando uma fila. a fila tem o mesmo nome da do emissor.
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
     System.out.println(" [*] Esperando recebimento de mensagens...");
 
+    //consumidor de mensagens
     Consumer consumer = new DefaultConsumer(channel) {
+      //método será executado ao chegar msg.
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
           throws IOException {
+        //mapear msg para string.
         String message = new String(body, "UTF-8");
         System.out.println(" [x] Mensagem recebida: '" + message + "'");
       }
     };
+    //o metodo handleDelivery vai ser chamado toda vez que houver mensagem na fila.
     channel.basicConsume(QUEUE_NAME, true, consumer);
   }
 }
